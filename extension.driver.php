fc53e2e5eba3432e8df30dbf345184fa8c48b958
Alannah Kearney <hi@alannahkearney.com>
2018-11-01 15:48:31 +1000

    Removed 'auto_generate' from install SQL. Added update() method to remove auto_generate column from existing installations.

--- extension.driver.php
+++ extension.driver.php
@@ -7,16 +7,24 @@
             Symphony::Database()->query("DROP TABLE `tbl_fields_uuid`");
         }
 
+        public function update()
+        {
+            return Symphony::Database()
+                ->query(
+                    "ALTER TABLE `tbl_fields_uuid` DROP COLUMN `auto_generate`;"
+            );
+        }
+
         public function install()
         {
-            return Symphony::Database()->query("
-                CREATE TABLE `tbl_fields_uuid` (
-                  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-                  `field_id` int(11) unsigned NOT NULL,
-                  `auto_generate` enum('yes','no') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'no',
-                  PRIMARY KEY (`id`),
-                  UNIQUE KEY `field_id` (`field_id`)
-                ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-			");
+            return Symphony::Database()
+                ->query(
+                    "CREATE TABLE IF NOT EXISTS `tbl_fields_uuid` (
+                      `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                      `field_id` int(11) unsigned NOT NULL,
+                      PRIMARY KEY (`id`),
+                      UNIQUE KEY `field_id` (`field_id`)
+                    ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+            );
         }
     }
